f8ac01fc8e27b3c906b40474ec80a5b23fb7b78f
cameron taggart <cameron.taggart@gmail.com>
2010-12-03 19:22:16 -0800

    Asynchronous Serial Port Communication with F#

---
+++ AsyncSerialPort/AsyncSerialPort.fs
@@ -0,0 +1,24 @@
+﻿module AsyncSerialPort
+
+open System.IO.Ports
+open System.Text
+
+type SerialPort with
+
+  member this.AsyncWriteLine(s:string) =
+    this.BaseStream.AsyncWrite(this.Encoding.GetBytes(s+"\n"))
+  
+  // expects a terminating line feed '\n'
+  member this.AsyncReadLine() =
+    async {
+      let sb = StringBuilder()
+      let bufferRef = ref (Array.zeroCreate<byte> this.ReadBufferSize)
+      let buffer = !bufferRef
+      let lastChr = ref 0uy
+      while !lastChr <> byte '\n' do
+        let! readCount = this.BaseStream.AsyncRead buffer
+        lastChr := buffer.[readCount-1]
+        sb.Append (this.Encoding.GetString(buffer.[0 .. readCount-1])) |> ignore
+      sb.Length <- sb.Length-1 // get rid of '\n'
+      return sb.ToString()
+    }
---
+++ AsyncSerialPort/Program.fs
@@ -0,0 +1,48 @@
+﻿module Program
+
+open System.Windows
+open System.Windows.Controls
+open System.IO.Ports
+open AsyncSerialPort
+
+type Form() as this =
+  inherit Window()
+
+  do
+    let sp = StackPanel()
+    this.Content <- sp
+    let tbRequest = TextBox();
+    tbRequest.Text <- "*IDN?"
+    let tbResponse = TextBlock()
+    let btn = Button(Content="Query Device")
+
+    sp.Children.Add tbRequest |> ignore
+    sp.Children.Add tbResponse |> ignore
+    sp.Children.Add btn |> ignore
+
+    let sp = new SerialPort(PortName="COM1", BaudRate=9600, DataBits=8, Parity=Parity.None, StopBits=StopBits.One);
+
+    // synchronous, blocks UI
+//    btn.Click.Add(fun _ ->
+//      sp.WriteLine tbRequest.Text
+//      let rsp = sp.ReadLine();
+//      tbResponse.Text <- rsp;
+//    )
+
+    // asynchronous, does not block UI
+    btn.Click.Add(fun _ ->
+      async {
+        do! sp.AsyncWriteLine tbRequest.Text
+        let! rsp = sp.AsyncReadLine()
+        tbResponse.Text <- rsp;
+      }
+      |> Async.StartImmediate
+    )
+
+    sp.Open()
+    ()
+
+
+[<System.STAThread>]
+do
+  Application().Run(Form()) |> ignore
